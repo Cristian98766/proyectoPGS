7c8c667255d0335614c55cfd605359e2f32263ca
Megamanchi0 <menlockfull@gmail.com>
2023-10-08 20:31:40 -0500

    Sprint 3

--- assets/views/javascripts/userPage.js
+++ assets/views/javascripts/userPage.js
@@ -4,13 +4,7 @@ const iconoMenu = toggle.querySelector("i")
 const enlacesMenu = document.querySelectorAll(".enlace")
 
 toggle.addEventListener("click", () => {
-    menuDashboard.classList.toggle("open")
-
-    if(iconoMenu.classList.contains("bx-menu")){
-        iconoMenu.classList.replace("bx-menu", "bx-x")
-    }else {
-        iconoMenu.classList.replace("bx-x", "bx-menu")
-    }
+    mostrarMenu();
 })
 
 enlacesMenu.forEach(enlace => {
@@ -19,3 +13,47 @@ enlacesMenu.forEach(enlace => {
         iconoMenu.classList.replace("bx-menu", "bx-x")
     })
 })
+
+function mostrarMenu() {
+    menuDashboard.classList.toggle("open");
+
+    if(iconoMenu.classList.contains("bx-menu")){
+        if ($(window).width() <= 700) {
+            $(".menu-dashboard").css("height", "100%");
+            $(".menu-dashboard").css("max-width", "300px");
+            $(".menu-dashboard").css("max-height", "100%");
+            $(".principal").css("max-height", "1700px");
+            $(".menu").css("display", "block");
+            $(".top-menu .logo, .input-search").css("display", "flex");
+        }
+        iconoMenu.classList.replace("bx-menu", "bx-x")
+    }else {
+        if ($(window).width() <= 700) {
+            $(".menu-dashboard").css("max-width", "60px");
+            $(".menu-dashboard").css("max-height", "70px");
+            $(".menu").css("display", "none");
+            $(".top-menu .logo, .input-search").css("display", "none");
+        }
+        iconoMenu.classList.replace("bx-x", "bx-menu")
+    }
+}
+
+    function handleResize() {
+        if ($(window).width() > 700) {
+            $(".menu-dashboard").css("height", "");
+            $(".menu-dashboard").css("max-width", "");
+            $(".menu-dashboard").css("max-height", "");
+            $(".principal").css("max-height", "");
+            $(".menu").css("display", "");
+            $(".top-menu .logo, .input-search").css("display", "");
+        }else if (!iconoMenu.classList.contains("bx-menu")) {
+            $(".menu-dashboard").css("height", "100%");
+            $(".menu-dashboard").css("max-width", "300px");
+            $(".menu-dashboard").css("max-height", "100%");
+            $(".principal").css("max-height", "1700px");
+            $(".menu").css("display", "block");
+            $(".top-menu .logo, .input-search").css("display", "flex");
+        }
+    }
+
+    $(window).resize(handleResize);
